c87657625d35dfa9d07776ce48c87156b1be4465
Harrix <sergienkoanton@mail.ru>
2013-10-17 23:44:35 +0800

    Обновление библиотек

--- Source/HarrixMathLibrary.cpp
+++ Source/HarrixMathLibrary.cpp
@@ -1,5 +1,5 @@
 //HarrixMathLibrary
-//Версия 3.23
+//Версия 3.25
 //Сборник различных математических функций и шаблонов с открытым кодом на языке C++.
 //https://github.com/Harrix/HarrixMathLibrary
 //Библиотека распространяется по лицензии Apache License, Version 2.0.
@@ -1467,6 +1467,142 @@ int MHL_ClassOfTestFunction(TypeOfTestFunction Type)
     return VMHL_Result;
 }
 //---------------------------------------------------------------------------
+int MHL_CountOfFitnessOfTestFunction_Binary(int Dimension)
+{
+    /*
+    Функция определяет количество вычислений целевой функции для тестовых задач для единообразного сравнения алгоритмов.
+    Обязательно вызвать один раз перед ее использованием функцию MHL_DefineTestFunction,
+    в которой определяется конкретный тип задачи оптимизации.
+    Для тестовых функций бинарного типа.
+    Входные параметры:
+     Dimension - размерность тестовой задачи. Может принимать значения: 20; 30; 40; 50; 60; 70; 80; 90; 100; 200.
+    Возвращаемое значение:
+     Количество вычислений целевой функции для тестовых задач.
+    */
+    int VMHL_Result = 0;
+
+    if (VMHL_TypeOfTestFunction==TestFunction_SumVector)
+    {
+        if (Dimension==20)  VMHL_Result = 225;
+        if (Dimension==30)  VMHL_Result = 400;
+        if (Dimension==40)  VMHL_Result = 576;
+        if (Dimension==50)  VMHL_Result = 784;
+        if (Dimension==60)  VMHL_Result = 1024;
+        if (Dimension==70)  VMHL_Result = 1296;
+        if (Dimension==80)  VMHL_Result = 1521;
+        if (Dimension==90)  VMHL_Result = 1764;
+        if (Dimension==100) VMHL_Result = 2025;
+        if (Dimension==200) VMHL_Result = 4761;
+    }
+
+    return VMHL_Result;
+}
+//---------------------------------------------------------------------------
+int MHL_CountOfFitnessOfTestFunction_Binary(int Dimension, TypeOfTestFunction Type)
+{
+    /*
+    Функция определяет количество вычислений целевой функции для тестовых задач для единообразного сравнения алгоритмов.
+    Обязательно вызвать один раз перед ее использованием функцию MHL_DefineTestFunction,
+    в которой определяется конкретный тип задачи оптимизации.
+    Для тестовых функций бинарного типа.
+    Входные параметры:
+     Dimension - размерность тестовой задачи. Может принимать значения: 2; 3; 4; 5; 10; 20; 30;
+     Type - тип тестовой функции.
+    Возвращаемое значение:
+     Количество вычислений целевой функции для тестовых задач.
+    */
+    int VMHL_Result = 0;
+
+    VMHL_TypeOfTestFunction = Type;
+
+    VMHL_Result = MHL_CountOfFitnessOfTestFunction_Binary(Dimension);
+
+    return VMHL_Result;
+}
+//---------------------------------------------------------------------------
+int MHL_CountOfFitnessOfTestFunction_Real(int Dimension)
+{
+    /*
+    Функция определяет количество вычислений целевой функции для тестовых задач для единообразного сравнения алгоритмов.
+    Обязательно вызвать один раз перед ее использованием функцию MHL_DefineTestFunction,
+    в которой определяется конкретный тип задачи оптимизации.
+	Для тестовых функций вещественного типа.
+    Входные параметры:
+     Dimension - размерность тестовой задачи. Может принимать значения: 2; 3; 4; 5; 10; 20; 30.
+    Возвращаемое значение:
+     Количество вычислений целевой функции для тестовых задач.
+    */
+    int VMHL_Result = 0;
+
+    if (VMHL_TypeOfTestFunction==TestFunction_Ackley)
+    {
+        if (Dimension==2)  VMHL_Result = 324;
+        if (Dimension==3)  VMHL_Result = 729;
+        if (Dimension==4)  VMHL_Result = 1225;
+        if (Dimension==5)  VMHL_Result = 1849;
+        if (Dimension==10) VMHL_Result = 5776;
+        if (Dimension==20) VMHL_Result = 15876;
+        if (Dimension==30) VMHL_Result = 28224;
+    }
+
+    if (VMHL_TypeOfTestFunction==TestFunction_ParaboloidOfRevolution)
+    {
+        if (Dimension==2)  VMHL_Result = 324;
+        if (Dimension==3)  VMHL_Result = 729;
+        if (Dimension==4)  VMHL_Result = 1225;
+        if (Dimension==5)  VMHL_Result = 1849;
+        if (Dimension==10) VMHL_Result = 5776;
+        if (Dimension==20) VMHL_Result = 15876;
+        if (Dimension==30) VMHL_Result = 28224;
+    }
+
+    if (VMHL_TypeOfTestFunction==TestFunction_Rastrigin)
+    {
+        if (Dimension==2)  VMHL_Result = 324;
+        if (Dimension==3)  VMHL_Result = 729;
+        if (Dimension==4)  VMHL_Result = 1225;
+        if (Dimension==5)  VMHL_Result = 1849;
+        if (Dimension==10) VMHL_Result = 5776;
+        if (Dimension==20) VMHL_Result = 15876;
+        if (Dimension==30) VMHL_Result = 28224;
+    }
+
+    if (VMHL_TypeOfTestFunction==TestFunction_Rosenbrock)
+    {
+        if (Dimension==2)  VMHL_Result = 324;
+        if (Dimension==3)  VMHL_Result = 729;
+        if (Dimension==4)  VMHL_Result = 1225;
+        if (Dimension==5)  VMHL_Result = 1849;
+        if (Dimension==10) VMHL_Result = 5776;
+        if (Dimension==20) VMHL_Result = 15876;
+        if (Dimension==30) VMHL_Result = 28224;
+    }
+
+    return VMHL_Result;
+}
+//---------------------------------------------------------------------------
+int MHL_CountOfFitnessOfTestFunction_Real(int Dimension, TypeOfTestFunction Type)
+{
+    /*
+    Функция определяет количество вычислений целевой функции для тестовых задач для единообразного сравнения алгоритмов.
+    Обязательно вызвать один раз перед ее использованием функцию MHL_DefineTestFunction,
+    в которой определяется конкретный тип задачи оптимизации.
+	Для тестовых функций вещественного типа.
+    Входные параметры:
+     Dimension - размерность тестовой задачи. Может принимать значения: 2; 3; 4; 5; 10; 20; 30;
+     Type - тип тестовой функции.
+    Возвращаемое значение:
+     Количество вычислений целевой функции для тестовых задач.
+    */
+    int VMHL_Result = 0;
+
+    VMHL_TypeOfTestFunction = Type;
+
+    VMHL_Result = MHL_CountOfFitnessOfTestFunction_Real(Dimension);
+
+    return VMHL_Result;
+}
+//---------------------------------------------------------------------------
 void MHL_DefineTestFunction(TypeOfTestFunction Type)
 {
     /*
@@ -1499,7 +1635,7 @@ int MHL_DimensionTestFunction_Binary(int i)
 
     //проверка
     if (i<0) i=0;
-    if (i>MHL_GetCountOfSubProblems_Binary()) i=MHL_GetCountOfSubProblems_Binary();
+    //if (i>=MHL_GetCountOfSubProblems_Binary()) i=MHL_GetCountOfSubProblems_Binary()-1;
 
     if (VMHL_TypeOfTestFunction==TestFunction_SumVector)
     {
@@ -1555,7 +1691,7 @@ int MHL_DimensionTestFunction_Real(int i)
 
     //проверка
     if (i<0) i=0;
-    if (i>MHL_GetCountOfSubProblems_Binary()) i=MHL_GetCountOfSubProblems_Binary();
+    //if (i>=MHL_GetCountOfSubProblems_Real()) i=MHL_GetCountOfSubProblems_Real()-1;
 
     if (VMHL_TypeOfTestFunction==TestFunction_Ackley)
     {

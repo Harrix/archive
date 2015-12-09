27b8e875ecf987e284c7c17daa2da7f9a096ef7f
Harrix <sergienkoanton@mail.ru>
2015-12-09 20:49:55 +0700

    Изменение названий функций

--- src/harrix-mardownfileinwordpress.php
+++ src/harrix-mardownfileinwordpress.php
@@ -13,35 +13,29 @@ require_once 'Michelf/Markdown.inc.php';
 
 use \Michelf\Markdown;
 
-add_shortcode('markdown','markdown_make');
-add_action('wp_head','add_highlight_js');
+add_shortcode('markdown','harrix_markdown_make');
+add_action('wp_head','harrix_add_highlight_js');
 
-function add_highlight_js() {
-echo "\n\n<link rel=\"stylesheet\" href=\"//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.0.0/styles/default.min.css\">\n";
-echo "<script src=\"//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.0.0/highlight.min.js\"></script>\n";
-echo "<script>hljs.initHighlightingOnLoad();</script>\n\n";
+function harrix_add_highlight_js() {
+    echo "\n\n<link rel=\"stylesheet\" href=\"//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.0.0/styles/default.min.css\">\n";
+    echo "<script src=\"//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.0.0/highlight.min.js\"></script>\n";
+    echo "<script>hljs.initHighlightingOnLoad();</script>\n\n";
 }
 
+function harrix_markdown_make( $atts, $content ) {
 
-function markdown_make( $atts, $content ){
-	// У нас в тегах [markdown][/markdown] будет располагаться ссылка на файл markdown.
-	// Например:
-	// [markdown]https://raw.github.com/Harrix/HarrixQtLibrary/master/README.md[/markdown]
-	
-    $yourfile = esc_url( $content ); // подчистим ссылку на всякий случай
-    $contents = file_get_contents( $yourfile ); // считаем файл по ссылке
-    $contents = do_shortcode( $contents ); // на всякий случай по тексту найдем шорткоды
+    $yourfile = esc_url( $content );
+    $contents = file_get_contents( $yourfile );
+    //$contents = do_shortcode( $contents );
 	
     if ($contents === false)
           {
-              $contents = "Не загрузили файл $yourfile";
+              $contents = "Do not downloaded $yourfile";
           }
           else
           {
- 
-              $contents = Markdown::defaultTransform($contents);//Непосредственно 
-              //$tr=array('<p><code>'=>'<pre class="lang:c++ decode:true " >','</code></p>'=>'</pre>',);
-			  
+              $contents = Markdown::defaultTransform($contents);
+		  
 			  $tr=array('<p><code>'=>'<pre><code style="cpp">','</code></p>'=>'</code></pre>');
               $contents=strtr($contents,$tr);
           }

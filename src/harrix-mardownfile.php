0c03110f92ddc1b2a0baa5fa64a5efb082e37fdd
Harrix <sergienkoanton@mail.ru>
2015-12-16 12:10:40 +0700

    wp_enqueue_script

--- src/harrix-mardownfile.php
+++ src/harrix-mardownfile.php
@@ -14,13 +14,23 @@ require_once 'Michelf/Markdown.inc.php';
 use \Michelf\Markdown;
 
 add_shortcode('markdown','harrix_markdown_make');
-add_action('wp_head','harrix_add_highlight_js');
+/*add_action('wp_head','harrix_add_highlight_js');
 
 function harrix_add_highlight_js() {
     echo "\n\n<link rel=\"stylesheet\" href=\"//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.0.0/styles/github.min.css\">\n";
     echo "<script src=\"//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.0.0/highlight.min.js\"></script>\n";
     echo "<script>hljs.initHighlightingOnLoad();</script>\n\n";
+}*/
+
+function wptuts_scripts_highlight()
+{
+    // Register the script like this for a plugin:
+    wp_register_script( 'highlight.js', plugins_url('/highlight.js') );
+ 
+    // For either a plugin or a theme, you can then enqueue the script:
+    wp_enqueue_script( 'highlight.js' );
 }
+add_action( 'wp_enqueue_scripts', 'wptuts_scripts_highlight' );
 
 function harrix_markdown_make( $atts, $content ) {
 

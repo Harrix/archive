78f24df7aad579f94c98ed7af59599d27f484b08
Harrix <sergienkoanton@mail.ru>
2015-12-16 14:11:37 +0700

    everything in order

--- src/harrix-mardownfile.php
+++ src/harrix-mardownfile.php
@@ -13,23 +13,23 @@ require_once 'Michelf/Markdown.inc.php';
 
 use \Michelf\Markdown;
 
+add_action( 'wp_enqueue_scripts', 'harrix_add_scripts_styles' );
+add_action('wp_head','harrix_load_highlight_js');
 add_shortcode('markdown','harrix_markdown_make');
-add_action('wp_head','harrix_add_highlight_js');
 
-function harrix_add_highlight_js() {
-    echo "<script>hljs.initHighlightingOnLoad();</script>\n\n";
-}
-
-function wptuts_scripts_highlight()
+function harrix_add_scripts_styles()
 {
     wp_register_script( 'highlight.js', plugin_dir_url( __FILE__ ) . 'highlight.min.js' );
     wp_enqueue_style( 'github.css', plugin_dir_url( __FILE__ ) . 'styles/github.css' );
     wp_enqueue_script( 'highlight.js' );
 }
-add_action( 'wp_enqueue_scripts', 'wptuts_scripts_highlight' );
 
-function harrix_markdown_make( $atts, $content ) {
+function harrix_load_highlight_js() {
+    echo "\n<script>hljs.initHighlightingOnLoad();</script>\n";
+}
 
+function harrix_markdown_make( $atts, $content ) 
+{
     $yourfile = esc_url( $content );
     $contents = file_get_contents( $yourfile );
 

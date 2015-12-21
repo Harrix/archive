71051b44c5e29ae4a1c7345b1e8946610882519d
Harrix <sergienkoanton@mail.ru>
2015-12-21 22:04:38 +0700

    Using the new parser of Markdown

--- src/harrix-markdownfile.php
+++ src/harrix-markdownfile.php
@@ -2,16 +2,14 @@
 /**
  * Plugin Name: Harrix MarkdownFile
  * Plugin URI: https://github.com/Harrix/Harrix-MarkdownFile
- * Description: Harrix MarkdownFile is a plugin to display Mardown files with syntax highlighting in Wordpress.
- * Version: 1.1
+ * Description: Harrix MarkdownFile is a plugin to display Markdown files with syntax highlighting in Wordpress.
+ * Version: 1.2
  * Author: Sergienko Anton
  * Author URI: http://harrix.org
  * License: MIT
  */
 
-require_once 'Michelf/Markdown.inc.php';
-
-use \Michelf\Markdown;
+require_once('Parsedown.php');
 
 add_action( 'wp_enqueue_scripts', 'harrix_add_scripts_styles' );
 add_action('wp_head','harrix_load_highlight_js');
@@ -24,7 +22,8 @@ function harrix_add_scripts_styles()
     wp_enqueue_script( 'highlight.js' );
 }
 
-function harrix_load_highlight_js() {
+function harrix_load_highlight_js() 
+{
     echo "\n<script>hljs.initHighlightingOnLoad();</script>\n";
 }
 
@@ -39,20 +38,11 @@ function harrix_markdown_make( $atts, $content )
           }
           else
           {
-              $contents = Markdown::defaultTransform($contents);
+              //$contents = Markdown::defaultTransform($contents);
+              
+              $Parsedown = new Parsedown();
 
-              $tr=array("<p><code>cpp\r\n"=>"<pre><code style=\"cpp\">",
-                        "<p><code>cpp\n"=>"<pre><code style=\"cpp\">",
-                        "<p><code>html\r\n"=>"<pre><code style=\"html\">",
-                        "<p><code>html\n"=>"<pre><code style=\"html\">",
-                        "<p><code>css\n"=>"<pre><code style=\"css\">",
-                        "<p><code>css\r\n"=>"<pre><code style=\"css\">",
-                       );
-              $contents=strtr($contents,$tr);
-
-              $tr=array('<p><code>'=>'<pre><code>',
-                        '</code></p>'=>'</code></pre>');
-              $contents=strtr($contents,$tr);
+$contents = $Parsedown->text($contents);
           }
 
     return $contents;

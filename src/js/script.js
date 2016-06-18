ac727cf0d4ee6cdc784a46a95d05cea784203458
Sergienko Anton <sergienkoanton@mail.ru>
2016-06-18 12:20:09 +0700

    PhotoSwipe

--- src/js/script.js
+++ src/js/script.js
@@ -31,8 +31,29 @@ $(document).ready(function(){
   })
 })
 
-;( function( $ ) {
+var pswpElement = document.querySelectorAll('.pswp')[0];
 
-	$( '.swipebox' ).swipebox();
+// build items array
+var items = [
+    {
+        src: 'https://placekitten.com/600/400',
+        w: 600,
+        h: 400
+    },
+    {
+        src: 'https://placekitten.com/1200/900',
+        w: 1200,
+        h: 900
+    }
+];
 
-} )( jQuery );
+// define options (if needed)
+var options = {
+    // optionName: 'option value'
+    // for example:
+    index: 0 // start at first slide
+};
+
+// Initializes and opens PhotoSwipe
+var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+gallery.init();

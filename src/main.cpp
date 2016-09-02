9d0e5b931772f79db99c10457a99c6a5bf4f82b3
Sergienko Anton <sergienkoanton@mail.ru>
2016-09-03 00:14:23 +0700

    Add QCommandLineParser.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
+#include <QCommandLineOption>
+#include <QCommandLineParser>
 #include <QCoreApplication>
+#include <QDebug>
+#include <QString>
+#include <QStringList>
+
 #include <iostream>
 
 using namespace std;
@@ -6,15 +12,20 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    QCoreApplication::setApplicationName("updt");
+    QCoreApplication::setApplicationVersion("1.0");
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription("Test helper");
+    parser.addHelpOption();
+    parser.addVersionOption();
 
     cout << "Hello, World1!\n";
 
-    QString S = QCoreApplication::arguments().at(0);
-    cout << "S "<< S.toStdString() <<"\n";
-
-    //S = QCoreApplication::arguments().at(1);
+    QString S;
     cout << "S "<< S.toStdString() <<"\n";
 
     //system("pause");
-    return a.exec();
+    //return a.exec();
+    return 0;
 }

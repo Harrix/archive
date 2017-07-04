bb65c59e004e3261cec9d0cf675f2e918e34a2bf
Sergienko Anton <sergienkoanton@mail.ru>
2017-07-04 15:47:22 +0300

    Modify QRFragment.

--- src/app/src/main/java/ru/uchimznaem/uchimznaemhelper/QRFragment.java
+++ src/app/src/main/java/ru/uchimznaem/uchimznaemhelper/QRFragment.java
@@ -1,29 +1,180 @@
 package ru.uchimznaem.uchimznaemhelper;
 
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import java.io.IOException;
+
 public class QRFragment extends Fragment {
 
+    private SurfaceView cameraView;
+    private TextView barcodeInfo;
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
+
+    private static final int CAMERA_PERMISSION_CAMERA = 0x000000;
 
     public QRFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_qr, container, false);
+        View v =  inflater.inflate(R.layout.fragment_qr, container, false);
+
+        if (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), android.Manifest.permission.CAMERA)) {
+
+            } else {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.CAMERA}, CAMERA_PERMISSION_CAMERA);
+            }
+        }
+
+        cameraView = (SurfaceView) v.findViewById(R.id.camera_view);
+        barcodeInfo = (TextView) v.findViewById(R.id.code_info);
+
+        getActivity().getPackageManager().hasSystemFeature (PackageManager.FEATURE_CAMERA_FLASH);
+
+        barcodeDetector =
+                new BarcodeDetector.Builder(getActivity().getApplicationContext())
+                        .setBarcodeFormats(Barcode.QR_CODE)
+                        .build();
+
+        cameraSource = new CameraSource
+                .Builder(getActivity().getApplicationContext(), barcodeDetector)
+                .setFacing(CameraSource.CAMERA_FACING_BACK)
+                .setRequestedFps(35.0f)
+                .setRequestedPreviewSize(960, 960)
+                .setAutoFocusEnabled(true)
+                .build();
+
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    cameraSource.start(cameraView.getHolder());
+                } catch (IOException ie) {
+                    Log.e("Harrix", ie.getMessage());
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+
+                if (barcodes.size() != 0) {
+                    barcodeInfo.post(new Runnable() {    // Use the post method of the TextView
+                        public void run() {
+                            barcodeInfo.setText(    // Update the TextView
+                                    barcodes.valueAt(0).displayValue
+                            );
+                        }
+                    });
+                }
+            }
+        });
+
+        return v;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[],
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case CAMERA_PERMISSION_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0]
+                        == PackageManager
+                        .PERMISSION_GRANTED) {
+
+                    Intent startMain = new Intent(getActivity(), MainActivity
+                            .class);
+                    startActivity(startMain);
+
+                } else {
+                    if (ContextCompat.checkSelfPermission(getActivity(),
+                            android.Manifest
+                                    .permission
+                                    .CAMERA)
+                            != PackageManager
+                            .PERMISSION_GRANTED) {
+
+                        // Should we show an explanation?
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                                android.Manifest
+                                        .permission
+                                        .CAMERA)) {
+
+                            // Show an explanation to the user *asynchronously* -- don't block
+                            // this thread waiting for the user's response! After the user
+                            // sees the explanation, try again to request the permission.
+
+                        } else {
+
+                            // No explanation needed, we can request the permission.
+
+                            ActivityCompat.requestPermissions(getActivity(),
+                                    new String[]{android.Manifest.permission
+                                            .CAMERA},
+                                    CAMERA_PERMISSION_CAMERA);
+
+                            // CAMERA_PERMISSION_CAMERA is an
+                            // app-defined int constant. The callback method gets the
+                            // result of the request.
+                        }
+                    }
+                }
+                return;
+            }
+        }
     }
 
 }

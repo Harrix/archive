252a817a6a9e1b7ded7912502ebcc51b7875c9cf
Sergienko Anton <sergienkoanton@mail.ru>
2017-07-04 13:52:27 +0300

    Add FragmentManager.

--- src/app/src/main/java/ru/uchimznaem/uchimznaemhelper/MainActivity.java
+++ src/app/src/main/java/ru/uchimznaem/uchimznaemhelper/MainActivity.java
@@ -3,6 +3,7 @@ package ru.uchimznaem.uchimznaemhelper;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +13,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private FrameLayout fragmentContainer;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +45,9 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        fragmentContainer = (FrameLayout)findViewById(R.id.fragmentContainer);
+        fragmentManager = getSupportFragmentManager();
     }
 
     @Override

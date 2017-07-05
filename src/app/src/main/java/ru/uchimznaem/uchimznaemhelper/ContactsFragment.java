ba97d3341cb96b09dc4a8d76badb1d654fd43f26
Ivan Petrushin <ivan.kiel@gmail.com>
2017-07-05 18:51:38 +0800

    Added contacts fragment

--- src/app/src/main/java/ru/uchimznaem/uchimznaemhelper/ContactsFragment.java
+++ src/app/src/main/java/ru/uchimznaem/uchimznaemhelper/ContactsFragment.java
@@ -1,19 +1,32 @@
 package ru.uchimznaem.uchimznaemhelper;
 
+import android.database.Cursor;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 /**
- * Created by teacher on 05.07.17.
+ * @author ipetrushin
  */
 
+
 public class ContactsFragment extends Fragment {
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        int[] fields = new int[]  {R.id.listid, R.id.listfullname, R.id.listroom, R.id.listphone};
+        String[] headers = new String[] {"id", "fullname", "room_number", "phone"};
+
         View root = inflater.inflate(R.layout.fragment_contacts, container, false);
+        ListView lvContacts = (ListView) root.findViewById(R.id.lvcontacts);
+        MainActivity activity = (MainActivity) getActivity();
+        Cursor c = activity.mDb.rawQuery("SELECT * FROM person ORDER by _id DESC", null);
+        SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(activity, R.layout.contacts_item, c, headers, fields, SimpleCursorAdapter.FLAG_AUTO_REQUERY);
+        lvContacts.setAdapter(cursorAdapter);
         return root;
     }
 }

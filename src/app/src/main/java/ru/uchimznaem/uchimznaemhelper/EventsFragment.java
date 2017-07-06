0ad441684529508a90c6179f80bc35e41d8a83f9
Vladimir Ulyanov <dedushka.lenin@gmail.com>
2017-07-06 17:47:11 +0800

    Events layout improvement

--- src/app/src/main/java/ru/uchimznaem/uchimznaemhelper/EventsFragment.java
+++ src/app/src/main/java/ru/uchimznaem/uchimznaemhelper/EventsFragment.java
@@ -11,7 +11,7 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 
 /**
- * @author ipetrushin
+ * @author ipetrushin, Vladimir Ulyanov
  */
 
 
@@ -19,14 +19,13 @@ public class EventsFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        int[] fields = new int[]{R.id.listid, R.id.listname, R.id.listroom, R.id.listbegin, R.id.listend};
-        String[] headers = new String[]{"_id", "name", "room", "begin", "end"};
+        int[] fields = new int[]{R.id.item_id, R.id.item_name, R.id.item_room};
+        String[] headers = new String[]{"_id", "nameroom", "begin"};
 
         View root = inflater.inflate(R.layout.fragment_events, container, false);
         ListView lvEvents = (ListView) root.findViewById(R.id.lvevents);
         MainActivity activity = (MainActivity) getActivity();
-        Cursor c = activity.mDb.rawQuery("SELECT id AS _id, name, room, datetime(start, 'unixepoch', 'localtime') AS begin, datetime(end, 'unixepoch', 'localtime') AS end  FROM events;", null);
-        Log.d("my", c.toString());
+        Cursor c = activity.mDb.rawQuery("SELECT id AS _id, name||' в кабинете ' || room AS nameroom, datetime(start, 'unixepoch', 'localtime') AS begin FROM events;", null);
         SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(activity, R.layout.events_item, c, headers, fields, SimpleCursorAdapter.FLAG_AUTO_REQUERY);
         lvEvents.setAdapter(cursorAdapter);
         return root;

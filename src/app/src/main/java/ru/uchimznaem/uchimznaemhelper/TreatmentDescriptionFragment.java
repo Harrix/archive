404a8309eafa313d3783a34d5f7e8f7459d8f2d4
Ivan Petrushin <ivan.kiel@gmail.com>
2017-07-06 16:50:39 +0800

    Added scrolling to treatment fragment

--- src/app/src/main/java/ru/uchimznaem/uchimznaemhelper/TreatmentDescriptionFragment.java
+++ src/app/src/main/java/ru/uchimznaem/uchimznaemhelper/TreatmentDescriptionFragment.java
@@ -1,6 +1,10 @@
 package ru.uchimznaem.uchimznaemhelper;
 
 
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,13 +13,16 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TreatmentDescriptionFragment extends Fragment {
 
-    private TextView treatmentName, treatmentDescription;
+    private TextView treatmentName, treatmentDescription, room;
     private ImageView image;
 
     private static final String TAG = "uz-helper";
@@ -29,21 +36,35 @@ public class TreatmentDescriptionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_room, container, false);
+        View v = inflater.inflate(R.layout.fragment_treatment, container, false);
+        image = (ImageView) v.findViewById(R.id.image);
+        treatmentName = (TextView) v.findViewById(R.id.name);
+        treatmentDescription = (TextView) v.findViewById(R.id.description);
+        room = (TextView) v.findViewById(R.id.room);
 
-        String message = getArguments().getString("message");
 
-        treatmentName = (TextView)v.findViewById(R.id.numberroom);
-        image = (ImageView)v.findViewById(R.id.image);
+        int id = Integer.parseInt(getArguments().getString("id"));
+        //id = 1;
+        MainActivity activity = (MainActivity) getActivity();
+        Cursor c = activity.mDb.rawQuery("SELECT name, description, room, image_filename FROM treatment WHERE id = " + id, null);
+        c.moveToFirst();
 
-        if (message.equals("hall")) {
-            treatmentName.setText(message);
-            image.setImageDrawable(getResources().getDrawable(R.drawable.room02));
-        }
+        if (c.getCount() > 0) {
+            treatmentName.setText(c.getString(c.getColumnIndex("name")));
+            treatmentDescription.setText(c.getString(c.getColumnIndex("description")));
+            room.setText(c.getString(c.getColumnIndex("room")));
+            String image_filename = c.getString(c.getColumnIndex("image_filename"));
 
-        if (message.equals("operating room")) {
-            treatmentName.setText(message);
-            image.setImageDrawable(getResources().getDrawable(R.drawable.room01));
+            try {
+                // loading image from filename
+                InputStream is = activity.getAssets().open(image_filename);
+                Bitmap bitmap = BitmapFactory.decodeStream(is);
+                image.setImageBitmap(bitmap);
+
+                treatmentName = (TextView) v.findViewById(R.id.numberroom);
+
+            } catch (IOException e) {}
+
         }
 
 

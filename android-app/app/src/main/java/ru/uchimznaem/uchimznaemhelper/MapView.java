974f4c9ce095722786debe6309c3c4f56665669a
Ivan Petrushin <ivan.kiel@gmail.com>
2018-03-01 19:38:56 +0300

    Fixed map movement

--- android-app/app/src/main/java/ru/uchimznaem/uchimznaemhelper/MapView.java
+++ android-app/app/src/main/java/ru/uchimznaem/uchimznaemhelper/MapView.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -13,8 +14,8 @@ import ru.uchimznaem.uchimznaemhelper.entities.Room;
 
 
 public class MapView extends View {
-    float x = 0, y = 0, touch_x = -1, touch_y = -1, x_offset = 0, y_offset = 0;
-    final int MAP_WIDTH = 2720, MAP_HEIGHT = 1130;
+    float x = 0, y = 0, touch_x = 0, touch_y = 0, x_offset = 0, y_offset = 0;
+    final int MAP_WIDTH = 1284 , MAP_HEIGHT = 534;
     float cWidth = 400, cHeight = 300;
 
     Drawable map;
@@ -22,6 +23,7 @@ public class MapView extends View {
 
         super(context, attrs);
         map = getContext().getDrawable(R.drawable.scheme);
+
 
     }
 
@@ -31,14 +33,12 @@ public class MapView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         cWidth = canvas.getWidth(); cHeight = canvas.getHeight();
-        //Paint p = new Paint();
-        //canvas.drawColor(Color.GRAY);
 
         float w = canvas.getWidth();
         float h = canvas.getHeight();
         int dx = (int) (touch_x - x);
         int dy = (int) (touch_y - y);
-        map.setBounds((int) (-dx + x_offset), (int) (-dy + y_offset), (int) (-dx + x_offset) + MAP_WIDTH, (int) (-dy + y_offset) + MAP_HEIGHT);
+        map.setBounds((int) (-dx + x_offset), (int) (-dy + y_offset), (int) (-dx + x_offset) + MAP_WIDTH, (int) (-dy + y_offset ) + MAP_HEIGHT);
 
         map.draw(canvas);
 
@@ -52,22 +52,17 @@ public class MapView extends View {
         x = event.getX(); y = event.getY();
 
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            x_offset = touch_x - x;
-            y_offset = touch_y - y;
             touch_x = x; touch_y = y;
         }
         else {
             if (event.getAction() == MotionEvent.ACTION_UP) {
 
-
-                //touch_x = 0;
-                //touch_y = 0;
-            }
-            else {
-
-
+                x_offset -= touch_x - x; touch_x = x;
+                y_offset -= touch_y - y; touch_y = y;
+                Log.d("my", String.format("x: %.1f, y: %.1f", x, y));
             }
         }
+
         invalidate();
         return true;
     }

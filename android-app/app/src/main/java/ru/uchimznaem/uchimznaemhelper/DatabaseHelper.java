033e3c0f92e2c3605c7fdee9970f4095d63fb788
Vladimir Ulyanov <dedushka.lenin@gmail.com>
2018-03-12 20:10:53 +0800

    DatabaseHelper обновлен

--- android-app/app/src/main/java/ru/uchimznaem/uchimznaemhelper/DatabaseHelper.java
+++ android-app/app/src/main/java/ru/uchimznaem/uchimznaemhelper/DatabaseHelper.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -12,6 +13,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String TAG = "DatabaseHelper";
     private static String DB_NAME = "helper.db";
     private static String DB_PATH = "";
     private static final int DB_VERSION = 4;
@@ -26,6 +28,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             DB_PATH = context.getApplicationInfo().dataDir + "/databases/";
         else
             DB_PATH = "/data/data/" + context.getPackageName() + "/databases/";
+        Log.d(TAG, "DB_PATH="+DB_PATH);
         this.mContext = context;
 
         copyDataBase();
@@ -33,6 +36,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         this.getReadableDatabase();
     }
 
+    /**
+     * Удаляет существующий файл базы данных и заменяет на новый файл из assets в случае если
+     * установлен флаг mNeedUpdate=true. По завершении сбрасывает флаг в false
+     *
+     * @throws IOException при невозможности выполнения оперкации с файлами
+     */
     public void updateDataBase() throws IOException {
         if (mNeedUpdate) {
             File dbFile = new File(DB_PATH + DB_NAME);
@@ -45,11 +54,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
     }
 
+    /**
+     * Проверяет наличие базыданных с указанным именем в константе DB_NAME
+     * @return в случае, если файл базы существует, возвращает true
+     */
     private boolean checkDataBase() {
         File dbFile = new File(DB_PATH + DB_NAME);
         return dbFile.exists();
     }
 
+    /**
+     * Копирует базу данных из assets при отсутствии файла база данных
+     */
     private void copyDataBase() {
         if (!checkDataBase()) {
             this.getReadableDatabase();
@@ -57,22 +73,31 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             try {
                 copyDBFile();
             } catch (IOException mIOException) {
-                throw new Error("ErrorCopyingDataBase");
+                throw new Error("ErrorCopyingDataBase", mIOException);
             }
         }
     }
 
+
+    /**
+     * Копирует с заменой файл базы данных из папки Assets
+     *
+     * @throws IOException
+     */
     private void copyDBFile() throws IOException {
         InputStream mInput = mContext.getAssets().open(DB_NAME);
-        //InputStream mInput = mContext.getResources().openRawResource(R.raw.info);
-        OutputStream mOutput = new FileOutputStream(DB_PATH + DB_NAME);
-        byte[] mBuffer = new byte[1024];
-        int mLength;
-        while ((mLength = mInput.read(mBuffer)) > 0)
-            mOutput.write(mBuffer, 0, mLength);
-        mOutput.flush();
-        mOutput.close();
-        mInput.close();
+        File target = new File(DB_PATH + DB_NAME);
+        OutputStream mOutput = new FileOutputStream(target);
+        try {
+            byte[] mBuffer = new byte[1024];
+            int mLength;
+            while ((mLength = mInput.read(mBuffer)) > 0)
+                mOutput.write(mBuffer, 0, mLength);
+        } finally {
+            mOutput.flush();
+            mOutput.close();
+            mInput.close();
+        }
     }
 
     public boolean openDataBase() throws SQLException {
@@ -80,6 +105,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return mDataBase != null;
     }
 
+    /**
+     * Закрывает соединение с базой данных.
+     */
     @Override
     public synchronized void close() {
         if (mDataBase != null)
@@ -87,13 +115,28 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         super.close();
     }
 
+    /**
+     * Выполняется при создании базы данных.
+     * Может использоваться для вызова SQL-запросов к базе данных при установке приложения.
+     * Обычно создает БД средствами команд CREATE TABLE и INSERT
+     * @param db база данных, созданная Android для данного приложения.
+     */
+
     @Override
     public void onCreate(SQLiteDatabase db) {
 
     }
 
+    /**
+     * Механизм обновления базы данных при обновлении приложения
+     *
+     * @param db база данных
+     * @param oldVersion номер текущей версии  приложения (старой)
+     * @param newVersion номер версии приложения до которого нужно обновиться (новой)
+     */
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // В случае обновления новой версии приложения, вся база данных уничтожается и заменяется на новую копию из Assets
         if (newVersion > oldVersion)
             mNeedUpdate = true;
     }

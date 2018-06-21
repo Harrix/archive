37f1da88ce1741ddad665d9298e8c0f861be12d0
Sergienko Anton <sergienkoanton@mail.ru>
2018-06-22 00:08:32 +0700

    Modify domain

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 
   QCoreApplication::setApplicationName("Harrix QMLComponents");
   QCoreApplication::setOrganizationName("Harrix");
-  QCoreApplication::setOrganizationDomain("org.harrix");
+  QCoreApplication::setOrganizationDomain("io.harrix");
 
   QGuiApplication app(argc, argv);
 

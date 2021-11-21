d731d5e921b42aa4b3837c2f616383d84d430e62
Sergienko Anton <sergienkoanton@mail.ru>
2021-11-21 17:27:46 +0300

    Refactor. Add fuction file_to_folder_of_same_name

--- file_to_folder_of_same_name.py
+++ file_to_folder_of_same_name.py
@@ -1,12 +1,17 @@
 from pathlib import Path
-import shutil
 
-path = input()
+def file_to_folder_of_same_name(path:str, extension:str) -> None:
+    for file in Path(path).glob("**/*"):
+        if file.is_file() and (file.suffix.lower() in [extension, ""]) :
+            path_new = path / Path(file.stem)
+            path_new.mkdir(parents=True, exist_ok=True)
+            Path.rename(file, path_new / Path(file.name))
 
-extension = ".stl"
 
-for file in Path(path).glob("**/*"):
-    if file.is_file() and file.suffix.lower() == extension:
-        path_new = path / Path(file.stem)
-        path_new.mkdir(parents=True, exist_ok=True)
-        Path.rename(file, path_new / Path(file.name))
+if __name__ == "__main__":
+    path = input()
+    if path:
+        extension = input()
+        file_to_folder_of_same_name(path, extension)
+    else:
+        file_to_folder_of_same_name("D:\\Downloads\\_3d", ".stl")

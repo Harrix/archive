36b21baa44f54931f838c473568471e5c046ffea
Sergienko Anton <sergienkoanton@mail.ru>
2022-04-17 18:14:14 +0300

    Docs. Add example and description

--- all_files_to_parent_dir.py
+++ all_files_to_parent_dir.py
@@ -1,3 +1,44 @@
+"""
+CLI console utility copies files from subdirectories to these folders
+for each folder in the parent directory.
+
+## Usage example
+
+Before:
+
+```text
+C:\test
+├─ folder1
+│  ├─ image.jpg
+│  ├─ sub1
+│  │  ├─ file1.txt
+│  │  └─ file2.txt
+│  └─ sub2
+│     ├─ file3.txt
+│     └─ file4.txt
+└─ folder2
+   └─ sub3
+      ├─ file6.txt
+      └─ sub4
+         └─ file5.txt
+```
+
+After:
+
+```text
+C:\test
+├─ folder1
+│  ├─ file1.txt
+│  ├─ file2.txt
+│  ├─ file3.txt
+│  ├─ file4.txt
+│  └─ image.jpg
+└─ folder2
+   ├─ file5.txt
+   └─ file6.txt
+```
+"""
+
 from pathlib import Path
 import shutil
 

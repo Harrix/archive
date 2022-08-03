148d122b146f3a93c810247f9ffb6de83097424c
Sergienko Anton <sergienkoanton@mail.ru>
2022-08-03 11:20:15 +0300

    Delete function add_author_book_evernote

--- src/specific__quotes.py
+++ src/specific__quotes.py
@@ -5,7 +5,6 @@ Scripts for the design of Markdown quotes.
 
 ```console
 python specific__quotes.py
-1
 D:/Dropbox/Notes/Quotes/Fiction
 ```
 
@@ -99,90 +98,6 @@ def add_author_book(filename: Path | str) -> None:
         print(f"No changes in {filename}")
 
 
-def add_author_book_evernote(filename: Path | str) -> None:
-    """
-    The function adds the author and the title of the book to the quotes from
-    Evernote and forms them as Markdown quotes.
-
-    Args:
-
-    - `filename` (Path | str): The filename of the Markdown File.
-
-    Returns:
-
-    - `None`.
-
-       Example file `C:/test/Name_Surname/Title_of_book/Title_of_book.md`:
-
-    ```markdown
-    # Title of book
-
-    |Modified title of book 1
-    Line 1.
-
-    Line 2.
-
-    ---
-
-    |Modified title of book 2
-    Line 3.
-
-    Line 4.
-    ```
-
-    After:
-
-    ```markdown
-    # Title of book
-
-    > Line 1.
-    >
-    > Line 2.
-    >
-    > _Name Surname - Modified title of book 1_
-
-    ---
-
-    > Line 3.
-    >
-    > Line 4.
-    >
-    > _Name Surname - Modified title of book 2_
-    ```
-    """
-    file = Path(filename)
-    if not file.is_file():
-        return
-    if file.suffix.lower() != ".md":
-        return
-    note_initial = file.read_text(encoding="utf8")
-    lines = note_initial.splitlines()
-
-    author = file.parts[-3].replace("-", " ")
-    title = lines[0].replace("# ", "")
-
-    lines = lines[1:-1] if lines[-1].strip() == "---" else lines[1:]
-    note = f"# {title}\n\n"
-    quotes = list(map(str.strip, filter(None, "\n".join(lines).split("\n---\n"))))
-
-    quotes_fix = []
-    for quote in quotes:
-        lines_quote = quote.splitlines()
-        if lines_quote[-1].startswith("> -- _"):
-            quotes_fix.append(quote)  # The quote has already been processed
-            continue
-        if lines_quote[0].startswith("|"):
-            title = lines_quote[0][1:]
-            del lines_quote[0]
-        quote_fix = "\n".join([f"> {line}".rstrip() for line in lines_quote])
-        quotes_fix.append(f"{quote_fix}\n>\n> -- _{author}, {title}_")
-    note += "\n\n---\n\n".join(quotes_fix) + "\n"
-    if note_initial != note:
-        file.write_text(note, encoding="utf8")
-        print(f"\x1b[34mFix {filename}\x1b[0m")
-    else:
-        print(f"No changes in {filename}")
-
 def modify_all_md_in_folder(path: Path | str, fn) -> None:
     """
     The function applies the function `add_author_book` to all Markdown files
@@ -206,26 +121,13 @@ def modify_all_md_in_folder(path: Path | str, fn) -> None:
 
 
 def main():
-    print(
-        """Select command
-    1 - add_author_book
-    2 - add_author_book_evernote
-    """
-    )
-    command = input("Input number of a command: ")
-    if command == "1":
-        path = input("Input path: ")
-        if Path(path).is_dir():
-            modify_all_md_in_folder(path, add_author_book)
-        else:
-            print("Invalid path.")
-    elif command == "2":
-        path = input("Input path: ")
-        if Path(path).is_dir():
-            modify_all_md_in_folder(path, add_author_book_evernote)
-        else:
-            print("Invalid path.")
+    path = input("Input path: ")
+    if Path(path).is_dir():
+        modify_all_md_in_folder(path, add_author_book)
+    else:
+        print("Invalid path.")
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    help(modify_all_md_in_folder)
